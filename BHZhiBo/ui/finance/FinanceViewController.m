70e0d507074362ad97d5305a49cf9f8ef5c12ea7
xubojoy <xubojoy1112@163.com>
2016-12-27 10:09:20 +0800

    优化

--- BHZhiBo/ui/finance/FinanceViewController.m
+++ BHZhiBo/ui/finance/FinanceViewController.m
@@ -19,7 +19,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-//    self.view.autoresizesSubviews = NO;
     [self initHeadView];
     [self loadWebView];
 }
