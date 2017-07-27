e2daa38c10a162c915bb689819005834106cd1ff
xubojoy <xubojoy1112@163.com>
2017-07-27 17:53:26 +0800

    优化

--- BHZhiBo/AppDelegate.m
+++ BHZhiBo/AppDelegate.m
@@ -21,6 +21,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+//    self.window.autoresizesSubviews = NO;
     [self.window makeKeyAndVisible];
     self.window.backgroundColor = [UIColor whiteColor];
     [self initShareSDK];
